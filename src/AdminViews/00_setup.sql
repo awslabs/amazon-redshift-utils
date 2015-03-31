d50b23c0b60a7ddb5ccfb04a6e3a46a1b913f038
Prabhuram Mohan <Prabhuram.Mohan@univarusa.com>
2015-03-31 06:53:37 +0000

    Changed the permission to select

--- src/AdminViews/00_setup.sql
+++ src/AdminViews/00_setup.sql
@@ -1,4 +1,4 @@
 create schema admin;
 
 grant all on schema admin to group public;
-grant all on all tables in schema admin to group public;
+grant select on all tables in schema admin to group public;
