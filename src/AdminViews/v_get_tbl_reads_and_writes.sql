45626ff881f973ecf13d1646d4d6bf47b4f3c2b1
Peter Van Bouwel <pbbouwel@amazon.com>
2016-11-03 21:05:22 +0000

    Improve formatting

--- src/AdminViews/v_get_tbl_reads_and_writes.sql
+++ src/AdminViews/v_get_tbl_reads_and_writes.sql
@@ -1,7 +1,7 @@
 --DROP VIEW admin.v_get_tbl_reads_and_writes;
 /**********************************************************************************************
 Purpose: View to get the READ and WRITE operations per table.  This view should be used with a
-filter that limits the output for query IDs and transaction IDs.  This view will help to see 
+filter that limits the output for query IDs and/or transaction IDs.  This view will help to see 
 what tables are operated on by transactions or queries.
 History:
 2016-11-03 pvbouwel Created
@@ -9,11 +9,11 @@ History:
 CREATE OR REPLACE VIEW admin.v_get_tbl_reads_and_writes
 AS
 WITH v_operations AS 
-  ( SELECT query, tbl, min(starttime) AS starttime, max(endtime) AS endtime,'R' AS operation FROM stl_scan GROUP BY query, tbl
+  ( SELECT query, tbl, min(starttime) AS starttime, max(endtime) AS endtime, 'R' AS operation FROM stl_scan GROUP BY query, tbl
       UNION ALL
-    SELECT query,tbl,min(starttime) AS starttime,max(endtime) AS endtime,'w' AS operation FROM stl_delete GROUP BY query,tbl
+    SELECT query, tbl, min(starttime) AS starttime, max(endtime) AS endtime, 'w' AS operation FROM stl_delete GROUP BY query, tbl
       UNION ALL
-    SELECT query,tbl,min(starttime) AS starttime,max(endtime) AS endtime,'W' AS operation FROM stl_insert GROUP BY query,tbl
+    SELECT query, tbl, min(starttime) AS starttime, max(endtime) AS endtime, 'W' AS operation FROM stl_insert GROUP BY query, tbl
   )
 SELECT
   sq.xid
