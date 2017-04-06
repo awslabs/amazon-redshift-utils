9ab9f1f004e7d3275d883e40772721b0846cbb4a
adedotua <adedotua@amazon.com>
2017-04-06 08:46:36 -0400

    Update user_to_be_dropped_objs.sql

--- src/AdminScripts/user_to_be_dropped_objs.sql
+++ src/AdminScripts/user_to_be_dropped_objs.sql
@@ -15,27 +15,26 @@ Notes:			Create prepared statement. Run it i.e execute find_drop_userobjs('<user
 History:
 2017-03-24 adedotua created
 2017-03-27 adedotua updated prepared statement name
+2017-04-06 adedotua improvements
 **********************************************************************************************/
 
-prepare find_drop_userobjs(varchar) as WITH
-pgu as (select usesysid,usename from pg_user where usename=$1),
-nc as (select oid,nspname from pg_namespace),
-pproc as (select oid,proowner,pronamespace from pg_proc),
-pgc as (select relnamespace,relname,relkind,relowner from pg_class)
+prepare find_drop_userobjs(varchar) as 
 select owner.objtype,owner.objowner,owner.userid,owner.schemaname,owner.objname from(
+-- Functions owned by the user
 select 'Function',pgu.usename,pgu.usesysid,nc.nspname,textin(regprocedureout(pproc.oid::regprocedure))
 from 
-pproc,pgu,nc 
-where pproc.pronamespace=nc.oid and pproc.proowner=pgu.usesysid and pproc.proowner>1
+pg_proc pproc,pg_user pgu,pg_namespace nc 
+where pproc.pronamespace=nc.oid and pproc.proowner=pgu.usesysid
 UNION ALL
-select 'Schema',pgu.usename,pgu.usesysid,null,pgn.nspname from pg_namespace pgn,pgu where pgn.nspowner=pgu.usesysid
+-- Databases owned by the user
+select 'Database',pgu.usename,pgu.usesysid,null,pgd.datname from pg_database pgd,pg_user pgu where pgd.datdba=pgu.usesysid
 UNION ALL
-select 
-case pgc.relkind
-when 'r' then 'Table'
-when 'v' then 'View'
-end,
-pgu.usename,pgu.usesysid,nc.nspname,pgc.relname
+-- Schemas owned by the user
+select 'Schema',pgu.usename,pgu.usesysid,null,pgn.nspname from pg_namespace pgn,pg_user pgu where pgn.nspowner=pgu.usesysid
+UNION ALL
+-- Tables or Views owned by the user
+select decode(pgc.relkind,'r','Table','v','View'),pgu.usename,pgu.usesysid,nc.nspname,pgc.relname
 from
-pgc,pgu,nc 
-where pgc.relnamespace=nc.oid and pgc.relkind in ('r','v') and pgu.usesysid=pgc.relowner) owner("objtype","objowner","userid","schemaname","objname");
+pg_class pgc,pg_user pgu,pg_namespace nc 
+where pgc.relnamespace=nc.oid and pgc.relkind in ('r','v') and pgu.usesysid=pgc.relowner) owner("objtype","objowner","userid","schemaname","objname")
+where owner.objowner = $1;
