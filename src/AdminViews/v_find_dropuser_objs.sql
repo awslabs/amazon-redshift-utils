85442d8be1f6c1d61e23bafbe33e05aa2963c738
adedotua <adedotua@amazon.com>
2017-04-06 08:54:11 -0400

    Update v_find_dropuser_objs.sql

--- src/AdminViews/v_find_dropuser_objs.sql
+++ src/AdminViews/v_find_dropuser_objs.sql
@@ -6,7 +6,7 @@ Columns -
 objtype:        Type of object user has privilege on. Object types are Function,Schema,
                 Table or View, Database, Language or Default ACL
 objowner:       Object owner 
-userid:			Owner user id
+userid:			    Owner user id
 schemaname:     Schema for the object
 objname:        Name of the object
 
@@ -14,28 +14,26 @@ Notes:
                 
 History:
 2017-03-27 adedotua created
+2017-04-06 adedotua improvements
 **********************************************************************************************/
 
-CREATE OR REPLACE VIEW v_find_dropuser_objs as WITH
-pgu as (select usesysid,usename from pg_user),
-nc as (select oid,nspname from pg_namespace),
-pproc as (select oid,proowner,pronamespace from pg_proc),
-pgc as (select relnamespace,relname,relkind,relowner from pg_class)
+CREATE OR REPLACE VIEW v_find_dropuser_objs as 
 select owner.objtype,owner.objowner,owner.userid,owner.schemaname,owner.objname from(
 -- Functions owned by the user
 select 'Function',pgu.usename,pgu.usesysid,nc.nspname,textin(regprocedureout(pproc.oid::regprocedure))
 from 
-pproc,pgu,nc 
-where pproc.pronamespace=nc.oid and pproc.proowner=pgu.usesysid and pproc.proowner>1
+pg_proc pproc,pg_user pgu,pg_namespace nc 
+where pproc.pronamespace=nc.oid and pproc.proowner=pgu.usesysid
 UNION ALL
--- Schemas created by the user
-select 'Database',pgu.usename,pgu.usesysid,null,pgd.datname from pg_database pgd,pgu where pgd.datdba=pgu.usesysid
+-- Databases owned by the user
+select 'Database',pgu.usename,pgu.usesysid,null,pgd.datname from pg_database pgd,pg_user pgu where pgd.datdba=pgu.usesysid
 UNION ALL
--- Schemas created by the user
-select 'Schema',pgu.usename,pgu.usesysid,null,pgn.nspname from pg_namespace pgn,pgu where pgn.nspowner=pgu.usesysid
+-- Schemas owned by the user
+select 'Schema',pgu.usename,pgu.usesysid,null,pgn.nspname from pg_namespace pgn,pg_user pgu where pgn.nspowner=pgu.usesysid
 UNION ALL
 -- Tables or Views owned by the user
-select case pgc.relkind when 'r' then 'Table' when 'v' then 'View' end,pgu.usename,pgu.usesysid,nc.nspname,pgc.relname
-from 
-pgc,pgu,nc 
-where pgc.relnamespace=nc.oid and pgc.relkind in ('r','v') and pgu.usesysid=pgc.relowner) owner("objtype","objowner","userid","schemaname","objname");
+select decode(pgc.relkind,'r','Table','v','View'),pgu.usename,pgu.usesysid,nc.nspname,pgc.relname
+from
+pg_class pgc,pg_user pgu,pg_namespace nc 
+where pgc.relnamespace=nc.oid and pgc.relkind in ('r','v') and pgu.usesysid=pgc.relowner) owner("objtype","objowner","userid","schemaname","objname")
+where owner.userid>1;
