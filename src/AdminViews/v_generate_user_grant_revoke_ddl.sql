76d259edbd51519983b58ade607d3a7fdc64a06d
fynixtech <fynixtech@users.noreply.github.com>
2017-12-17 13:07:47 -0500

    Update v_generate_user_grant_revoke_ddl.sql

--- src/AdminViews/v_generate_user_grant_revoke_ddl.sql
+++ src/AdminViews/v_generate_user_grant_revoke_ddl.sql
@@ -12,6 +12,7 @@ username:       User/Group the privilege is granted to
 objtype:        Type of object user has privilege on. Object types are Function,Schema,
                 Table or View, Database, Language or Default ACL
 ddltype:        Type of ddl generated i.e grant or revoke
+sequence:	Sequence number to order the DDLs by hierarchy
 ddl:            DDL text
 
 Notes:           
@@ -20,18 +21,21 @@ History:
 2017-07-11 	adedotua created
 2017-07-17 	adedotua added comments to account for difference between grantor and owner of 
 		object. Also added schemaname and objname columns.
+2017-12-17	adedotua added sequence column to order grant or revoke DDLs in order of heirarchy.
+2017-12-17	adedotua added schema name admin
+		
 		
 		
 Steps to revoking grants before dropping a user:
 
 1. Find all grants by granted by user to drop and regrant them as another user (superuser preferably).
-select ddl from v_generate_user_grant_revoke_ddl where grantor='<username>' and ddltype='grant' and objtype <>'Default ACL';
+select ddl from v_generate_user_grant_revoke_ddl where grantor='<username>' and ddltype='grant' and objtype <>'Default ACL' order by sequence;
 
 2. Find all grants granted to user to drop and revoke them.
-select ddl from v_generate_user_grant_revoke_ddl where ddltype='revoke' and (username='<username>' or grantor='<username>');		
+select ddl from v_generate_user_grant_revoke_ddl where ddltype='revoke' and (username='<username>' or grantor='<username>') order by sequence;		
 ************************************************************************************************************************/
 
-CREATE OR REPLACE VIEW v_generate_user_grant_revoke_ddl as
+CREATE OR REPLACE VIEW admin.v_generate_user_grant_revoke_ddl as
 WITH 
 grantor as (select usesysid,usename from pg_user),
 schemas as (select oid,nspname,nspacl,nspowner from pg_namespace),
@@ -59,7 +63,7 @@ UNION ALL
 SELECT 'USAGE'::character varying as type)
 select * from (
 -- Functions grants
-select pg_get_userbyid(c.proowner),sc.nspname,textin(regprocedureout(c.oid::regprocedure)),g.usename,u.usename,'Function','grant',
+select pg_get_userbyid(c.proowner),sc.nspname,textin(regprocedureout(c.oid::regprocedure)),g.usename,u.usename,'Function','grant',2,
 'grant execute on function '||sc.nspname||'.'||textin(regprocedureout(c.oid::regprocedure))||
 CASE WHEN u.usesysid>1 THEN ' to ' ELSE ' to group ' END||u.usename||
 CASE WHEN aclcontains(c.proacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,'EXECUTE',true)) THEN ' with grant option;' ELSE ';' END::text
@@ -67,14 +71,14 @@ from pg_proc c join schemas sc on c.pronamespace=sc.oid,grantor g,grantee u
 where  aclcontains(c.proacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,'EXECUTE',false))
 UNION ALL
 -- Functions revokes
-select pg_get_userbyid(c.proowner),sc.nspname,textin(regprocedureout(c.oid::regprocedure)),g.usename,u.usename,'Function','revoke',
+select pg_get_userbyid(c.proowner),sc.nspname,textin(regprocedureout(c.oid::regprocedure)),g.usename,u.usename,'Function','revoke',1,
 'revoke execute on function '||sc.nspname||'.'||textin(regprocedureout(c.oid::regprocedure))||
 CASE WHEN u.usesysid>1 THEN ' from ' ELSE ' from group ' END||u.usename||';'||CASE WHEN g.usename <> pg_get_userbyid(c.proowner) THEN ' -- revoke this object as '||g.usename ELSE '' END::text
 from pg_proc c join schemas sc on c.pronamespace=sc.oid,grantor g,grantee u
 where  aclcontains(c.proacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,'EXECUTE',false)) 
 UNION ALL
 -- Language grants
-select null,null,c.lanname,g.usename,u.usename,'Language','grant',
+select null,null,c.lanname,g.usename,u.usename,'Language','grant',1,
 'grant usage on language '||c.lanname||
 CASE WHEN u.usesysid>1 THEN ' to ' ELSE ' to group ' END||u.usename||
 CASE WHEN aclcontains(c.lanacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,'USAGE',true)) THEN ' with grant option;' ELSE ';' END::text
@@ -82,14 +86,14 @@ from pg_language c,grantor g,grantee u
 where aclcontains(c.lanacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,'USAGE',false)) 
 UNION ALL
 -- Language revokes
-select null,null,c.lanname,g.usename,u.usename,'Language','revoke',
+select null,null,c.lanname,g.usename,u.usename,'Language','revoke',2,
 'revoke usage on language '||c.lanname||
 CASE WHEN u.usesysid>1 THEN ' from ' ELSE ' from group ' END||u.usename||';'||CASE WHEN (g.usename <> current_user) and (g.usename<>'rdsdb') THEN ' -- revoke this object as '||g.usename ELSE '' END::text
 from pg_language c,grantor g,grantee u
 where aclcontains(c.lanacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,'USAGE',false)) 
 UNION ALL
 --Tables grants
-select pg_get_userbyid(c.relowner),sc.nspname,c.relname,g.usename,u.usename,case c.relkind WHEN 'r' THEN 'Table' WHEN 'v' THEN 'View' END,'grant',
+select pg_get_userbyid(c.relowner),sc.nspname,c.relname,g.usename,u.usename,case c.relkind WHEN 'r' THEN 'Table' WHEN 'v' THEN 'View' END,'grant',2,
 'grant '||t.type||' on '||sc.nspname||'.'||c.relname||
 CASE WHEN u.usesysid>1 THEN ' to ' ELSE ' to group ' END||u.usename||
 CASE WHEN aclcontains(c.relacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,t.type,true)) THEN ' with grant option;' ELSE ';' END::text
@@ -97,7 +101,7 @@ from pg_class c join schemas sc on c.relnamespace=sc.oid, grantor g,grantee u,ta
 where aclcontains(c.relacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,t.type,false)) 
 UNION ALL
 --Tables revokes
-select distinct pg_get_userbyid(c.relowner),sc.nspname,c.relname,g.usename,u.usename,'Table/View','revoke',
+select distinct pg_get_userbyid(c.relowner),sc.nspname,c.relname,g.usename,u.usename,'Table/View','revoke',1,
 'revoke all on '||sc.nspname||'.'||c.relname||
 CASE WHEN u.usesysid>1 THEN ' from ' ELSE ' from group ' END||u.usename||';'||CASE WHEN g.usename <> pg_get_userbyid(c.relowner) THEN ' -- revoke this object as '||g.usename ELSE '' END::text
 from pg_class c join schemas sc on c.relnamespace=sc.oid, grantor g,grantee u
@@ -113,7 +117,7 @@ UNION ALL
 select 1 WHERE aclcontains(c.relacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,'REFERENCES',false)))
 UNION ALL
 -- Schema grants
-select pg_get_userbyid(c.nspowner),null,c.nspname,g.usename,u.usename,'Schema','grant',
+select pg_get_userbyid(c.nspowner),null,c.nspname,g.usename,u.usename,'Schema','grant',1,
 'grant '||s.type||' on schema '||c.nspname||
 CASE WHEN u.usesysid>1 THEN ' to ' ELSE ' to group ' END||u.usename||
 CASE WHEN aclcontains(c.nspacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,s.type,true)) THEN ' with grant option;' ELSE ';' END::text
@@ -121,7 +125,7 @@ from pg_namespace c, grantor g,grantee u,schemaprivs s
 where  aclcontains(c.nspacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,s.type,false))
 UNION ALL
 -- Schema revokes
-select pg_get_userbyid(c.nspowner),null,c.nspname,g.usename,u.usename,'Schema','revoke',
+select pg_get_userbyid(c.nspowner),null,c.nspname,g.usename,u.usename,'Schema','revoke',2,
 'revoke all on schema '||c.nspname||
 CASE WHEN u.usesysid>1 THEN ' from ' ELSE ' from group ' END||u.usename||';'||CASE WHEN g.usename <> pg_get_userbyid(c.nspowner) THEN ' -- revoke this object as '||g.usename ELSE '' END::text
 from pg_namespace c, grantor g,grantee u
@@ -131,7 +135,7 @@ UNION ALL
 select 1 where aclcontains(c.nspacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,'USAGE',false)))
 UNION ALL
 -- Database grants
-select pg_get_userbyid(c.datdba),null,c.datname,g.usename,u.usename,'Database','grant',
+select pg_get_userbyid(c.datdba),null,c.datname,g.usename,u.usename,'Database','grant',0,
 'grant '||d.type||' on database '||c.datname||
 CASE WHEN u.usesysid>1 THEN ' to ' ELSE ' to group ' END||u.usename||
 CASE WHEN aclcontains(c.datacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,d.type,true)) THEN ' with grant option;' ELSE ';' END::text 
@@ -139,7 +143,7 @@ from pg_database c, grantor g,grantee u,dbprivs d
 where  aclcontains(c.datacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,d.type,false))
 UNION ALL
 -- Database revokes
-select pg_get_userbyid(c.datdba),null,c.datname,g.usename,u.usename,'Database','revoke',
+select pg_get_userbyid(c.datdba),null,c.datname,g.usename,u.usename,'Database','revoke',3,
 'revoke all on database '||c.datname||
 CASE WHEN u.usesysid>1 THEN ' from ' ELSE ' from group ' END||u.usename||';'||CASE WHEN g.usename <> pg_get_userbyid(c.datdba) THEN ' -- revoke this object as '||g.usename ELSE '' END::text
 from pg_database c, grantor g,grantee u 
@@ -149,7 +153,7 @@ UNION ALL
 select 1 where aclcontains(c.datacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,'TEMP',false)))
 UNION ALL
 -- Default ACL grants
-select pg_get_userbyid(c.defacluser),sc.nspname,decode(c.defaclobjtype,'r','Tables','f','Functions'),g.usename,u.usename,'Default ACL','grant',
+select pg_get_userbyid(c.defacluser),sc.nspname,decode(c.defaclobjtype,'r','Tables','f','Functions'),g.usename,u.usename,'Default ACL','grant',3,
 'alter default privileges for user '||pg_get_userbyid(c.defacluser)||
 CASE WHEN c.defaclnamespace >0 THEN ' in schema '||sc.nspname ELSE '' END||
 ' grant '||t.type||' on '||decode(c.defaclobjtype,'r','tables','f','functions')||
@@ -159,7 +163,7 @@ from pg_default_acl c left join schemas sc on c.defaclnamespace=sc.oid, grantor 
 where  aclcontains(c.defaclacl, makeaclitem(u.usesysid,u.grosysid,g.usesysid,t.type,false))
 UNION ALL
 -- Default ACL revokes
-select pg_get_userbyid(c.defacluser),sc.nspname,decode(c.defaclobjtype,'r','Tables','f','Functions'),g.usename,u.usename,'Default ACL','revoke',
+select pg_get_userbyid(c.defacluser),sc.nspname,decode(c.defaclobjtype,'r','Tables','f','Functions'),g.usename,u.usename,'Default ACL','revoke',0,
 'alter default privileges for user '||pg_get_userbyid(c.defacluser)||
 CASE WHEN c.defaclnamespace >0 THEN ' in schema '||sc.nspname ELSE '' END||
 ' revoke all on '||decode(c.defaclobjtype,'r','tables','f','functions')||
@@ -181,7 +185,7 @@ and pg_get_userbyid(c.defacluser)<>u.usename
 and array_to_string(c.defaclacl,'')<>('=X/'||pg_get_userbyid(c.defacluser))
 UNION ALL
 --Default ACL grants with empty acl
-select pg_get_userbyid(c.defacluser),sc.nspname,decode(c.defaclobjtype,'r','Tables','f','Functions'),null,pg_get_userbyid(c.defacluser),'Default ACL','revoke',
+select pg_get_userbyid(c.defacluser),sc.nspname,decode(c.defaclobjtype,'r','Tables','f','Functions'),null,pg_get_userbyid(c.defacluser),'Default ACL','revoke',0,
 'alter default privileges for user '||pg_get_userbyid(c.defacluser)||
 CASE WHEN c.defaclnamespace >0 THEN ' in schema '||sc.nspname ELSE '' END||
 ' grant all on '||
@@ -191,4 +195,4 @@ from pg_default_acl c left join schemas sc on c.defaclnamespace=sc.oid
 where EXISTS (select 1 where c.defaclacl='{}'::aclitem[]
 UNION ALL
 select 1 WHERE array_to_string(c.defaclacl,'')=('=X/'||pg_get_userbyid(c.defacluser)))
-)userddl("objowner","schemaname","objname","grantor","username","objtype","ddltype","ddl") where (username<>objowner or objtype='Default ACL' or objowner is null or grantor is null) and username<>'rdsdb';
+)userddl("objowner","schemaname","objname","grantor","username","objtype","ddltype","sequence","ddl") where (username<>objowner or objtype='Default ACL' or objowner is null or grantor is null) and username<>'rdsdb';
