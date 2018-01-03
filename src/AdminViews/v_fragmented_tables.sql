0f06ae75d6f0918ba2c32a059b7868ad11cfcc92
adedotua <adedotua@amazon.com>
2018-01-02 22:36:40 -0500

    Update v_fragmented_tables.sql

--- src/AdminViews/v_fragmented_tables.sql
+++ src/AdminViews/v_fragmented_tables.sql
@@ -7,7 +7,11 @@ History:
 **********************************************************************************************/ 
 
 CREATE VIEW admin.v_fragmented_tables as
-select tbl,trim(datname) as dbname,trim("name") as tablename,temp,backup 
+select tbl,
+trim(datname) as dbname,
+trim("name") as tablename,
+temp,
+backup 
 from (select tbl,floor(log(num_values)) from stv_blocklist where col=0 and num_values>0 and tbl > 1 group by 1,2) a 
 join (select db_id,"name",id,temp,backup from stv_tbl_perm where slice=0) b on a.tbl=b.id 
 join pg_database c on b.db_id=c.oid group by 1,2,3,4,5 having count(*) > 2 order by count(*) desc;
