6266810e0d06f310fb4c12ab4dcc13aead7a6f7d
IanMeyers <meyersi@amazon.com>
2018-02-23 17:53:44 +0000

    Now actually added pgpasslib dependencies

--- src/RedshiftAutomation/build.sh
+++ src/RedshiftAutomation/build.sh
@@ -51,4 +51,4 @@ echo "Imported Redshift Advance Monitoring"
 cd -
 
 # build the combined lambda package
-zip -r $ARCHIVE *.py config.json .pgpass ../aws_utils.py ../config_constants.py lib/AnalyzeVacuumUtility lib/ColumnEncodingUtility lib/SystemTablePersistence lib/pg8000 lib/shortuuid lib/amazon-redshift-monitoring/redshift_monitoring.py lib/amazon-redshift-monitoring/sql/ lib/amazon-redshift-monitoring/monitoring-queries.json
+zip -r $ARCHIVE *.py config.json .pgpass ../aws_utils.py ../config_constants.py lib/AnalyzeVacuumUtility lib/ColumnEncodingUtility lib/SystemTablePersistence lib/pg8000* lib/shortuuid* lib/pgpasslib* lib/amazon-redshift-monitoring/redshift_monitoring.py lib/amazon-redshift-monitoring/sql/ lib/amazon-redshift-monitoring/monitoring-queries.json
