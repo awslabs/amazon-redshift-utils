e7f4eb9fd6f875a8a87d0c32b97192c0d79c539e
Alex Burck <me@alexburck.com>
2018-03-30 15:08:58 -0500

    Add logic to add AUTHORIZATION clause

--- src/AdminViews/v_generate_schema_ddl.sql
+++ src/AdminViews/v_generate_schema_ddl.sql
@@ -4,10 +4,27 @@ Purpose: View to get the DDL for schemas.
 History:
 2014-02-11 jjschmit Created
 2018-01-15 pvbouwel Add QUOTE_IDENT for namespace literal
+2018-03-30 burck1 Add logic to add AUTHORIZATION clause
+Notes:
+If you receive the error
+	[Amazon](500310) Invalid operation: cannot change data type of view column "ddl";
+then you must drop the view and re-create it using
+	DROP VIEW admin.v_generate_schema_ddl;
 **********************************************************************************************/
 CREATE OR REPLACE VIEW admin.v_generate_schema_ddl
 AS
-SELECT nspname AS schemaname, 'CREATE SCHEMA ' + QUOTE_IDENT(nspname) + ';' AS ddl FROM pg_catalog.pg_namespace WHERE nspowner >= 100 ORDER BY nspname
+SELECT
+	nspname AS schemaname,
+	'CREATE SCHEMA ' + QUOTE_IDENT(nspname) +
+		CASE
+		WHEN nspowner > 100
+		THEN ' AUTHORIZATION ' + QUOTE_IDENT(pg_user.usename)
+		ELSE ''
+		END
+		+ ';' AS ddl
+FROM pg_catalog.pg_namespace as pg_namespace
+LEFT OUTER JOIN pg_catalog.pg_user pg_user
+ON pg_namespace.nspowner=pg_user.usesysid
+WHERE nspowner >= 100
+ORDER BY nspname
 ;
-
-
