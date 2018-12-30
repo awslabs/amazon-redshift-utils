37d9039b8e31c3e5246221a924b3504e169cb928
adedotua <adedotua@amazon.com>
2018-12-30 03:51:50 -0500

    Update v_vacuum_summary.sql
    fixed join condition to make vacuum on dropped tables visible

--- src/AdminViews/v_vacuum_summary.sql
+++ src/AdminViews/v_vacuum_summary.sql
@@ -5,16 +5,17 @@ Purpose:      View to flatten stl_vacuum table and provide details like vacuum s
 History:
 2017-12-24 adedotua created
 2018-12-22 adedotua updated view to account for background auto vacuum process  
+2018-12-30 adedotua fixed join condition to make vacuum on dropped tables visible
 **********************************************************************************************/ 
 
 CREATE OR REPLACE VIEW admin.v_vacuum_summary as SELECT a.userid,
        a.xid,
-       BTRIM((d.datname)::TEXT) AS database_name,
+       BTRIM(d.datname) AS database_name,
        a.table_id,
-       BTRIM((c.name)::TEXT) AS tablename,
-       BTRIM((a.status)::TEXT) AS vac_start_status,
+       BTRIM(c.name) AS tablename,
+       BTRIM(a.status) AS vac_start_status,
        CASE
-         WHEN (a.status ilike 'skipped%'::TEXT) THEN 'Skipped'::TEXT
+         WHEN (a.status ilike 'skipped%'::TEXT) THEN null
          WHEN (f.xid IS NOT NULL) THEN 'Running'::TEXT
          WHEN (b.status IS NULL) THEN 'Failed'::TEXT
          ELSE BTRIM((b.status)::TEXT)
@@ -34,36 +35,12 @@ CREATE OR REPLACE VIEW admin.v_vacuum_summary as SELECT a.userid,
        a."blocks" AS vac_start_blocks,
        b."blocks" AS vac_end_blocks,
        (b."blocks" - a."blocks") AS vac_block_diff,
-       COALESCE(e.empty_blk_cnt,(0)::BIGINT,e.empty_blk_cnt) AS empty_blk_cnt
-       FROM ((((((SELECT stl_vacuum.userid,
-                  stl_vacuum.xid,
-                  stl_vacuum.table_id,
-                  stl_vacuum.status,
-                  stl_vacuum."rows",
-                  stl_vacuum.sortedrows,
-                  stl_vacuum."blocks",
-                  stl_vacuum.max_merge_partitions,
-                  stl_vacuum.eventtime
-           FROM stl_vacuum
-           WHERE (stl_vacuum.status not ilike '%Finished%')) a 
-           LEFT JOIN (SELECT stl_vacuum.userid,
-           stl_vacuum.xid,
-           stl_vacuum.table_id,
-           stl_vacuum.status,
-           stl_vacuum."rows",
-           stl_vacuum.sortedrows,
-           stl_vacuum."blocks",
-           stl_vacuum.max_merge_partitions,
-           stl_vacuum.eventtime
-              FROM stl_vacuum
-              WHERE (stl_vacuum.status ilike '%Finished%')) b USING (xid,table_id)) 
-              LEFT JOIN (SELECT stv_tbl_perm.id,stv_tbl_perm.name,stv_tbl_perm.db_id
-                  FROM stv_tbl_perm WHERE (stv_tbl_perm.slice = 0)) c ON ((a.table_id = c.id))) 
-              JOIN pg_database d ON (((c.db_id)::OID = d.oid))) 
-                  LEFT JOIN (SELECT stv_blocklist.tbl,COUNT(*) AS empty_blk_cnt
-                  FROM stv_blocklist
-                  WHERE (stv_blocklist.num_values = 0) GROUP BY stv_blocklist.tbl) e ON ((a.table_id = e.tbl))) 
-             LEFT JOIN (SELECT svv_transactions.xid
-              FROM svv_transactions
-              WHERE ((svv_transactions.lockable_object_type)::TEXT = 'transactionid'::TEXT)) f USING (xid))
+       NVL(e.empty_blk_cnt,0) AS empty_blk_cnt
+       FROM 
+       (SELECT * FROM stl_vacuum WHERE status not ilike '%Finished%') a 
+       LEFT JOIN stl_vacuum b on a.xid=b.xid and a.table_id=b.table_id and a.eventtime < b.eventtime 
+       LEFT JOIN (SELECT id,name,db_id FROM stv_tbl_perm WHERE slice = 0) c ON a.table_id = c.id
+       LEFT JOIN pg_database d ON c.db_id::oid = d.oid
+       LEFT JOIN (SELECT tbl,COUNT(*) AS empty_blk_cnt FROM stv_blocklist WHERE num_values = 0 GROUP BY tbl) e ON a.table_id = e.tbl 
+       LEFT JOIN (SELECT xid FROM svv_transactions WHERE lockable_object_type = 'transactionid') f on a.xid=f.xid
 ORDER BY a.xid;
