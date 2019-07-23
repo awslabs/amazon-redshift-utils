ae5e1e2cd25cae811d6103c365ba2d63bac293e2
Styerp <35037768+Styerp@users.noreply.github.com>
2019-07-23 15:57:23 -0700

    Update v_generate_external_tbl_ddl.sql

--- src/AdminViews/v_generate_external_tbl_ddl.sql
+++ src/AdminViews/v_generate_external_tbl_ddl.sql
@@ -59,7 +59,7 @@ CREATE OR REPLACE VIEW admin.v_generate_external_tbl_ddl AS
                  WHERE part_key > 1
              UNION ALL
              SELECT ')'                 AS ddl
-                  , 1000000 + part_key + columnnum                                      AS seq
+                  , 99999                                      AS seq
                   , schemaname
                   , tablename
                  FROM svv_external_columns
