85a0e4468ff4d6cc4ecc72bd007f0240422f4db3
Dinesh Kumar <dinkumd@amazon.com>
2022-02-02 05:32:14 +0000

    Aqua_eligible_testqueries.sql

--- src/BlogContent/AQUA-Evaluation/Aqua_eligible_testqueries.sql
+++ src/BlogContent/AQUA-Evaluation/Aqua_eligible_testqueries.sql
@@ -1,34 +1,34 @@
 /* These are some of aqua eligible test queries*/
 select  count(*) from amazon_reviews 
-where product_title SIMILAR TO  '%lap%' or
-      product_title SIMILAR TO  '%iph%' or 
-      product_title SIMILAR TO  '%soa%' or 
-      product_title SIMILAR TO '%nice%' or 
-      product_title SIMILAR TO '%hope%';
+where product_title similar to '%lap%' or
+      product_title similar to '%iph%' or 
+      product_title similar to '%soa%' or 
+      product_title similar to '%nice%' or 
+      product_title similar to '%hope%';
 
-select count(*) from amazon_reviews where product_title SIMILAR TO '%lap%';
+select count(*) from amazon_reviews where product_title similar to '%lap%';
 
-select count(*) from amazon_reviews where product_title SIMILAR TO '%lap%' group by star_rating order by star_rating desc;
+select count(*) from amazon_reviews where product_title similar to '%lap%' group by star_rating order by star_rating desc;
 
 select count(customer_id), count(review_ID) from amazon_reviews where  review_body like '%one%car%';
 
 select count(*) from amazon_reviews where product_title ilike '%lap%' or product_title ilike '%e%|%E%' or customer_ID like '3%__%45__3';
 
-select customer_id , product_title , review_id  from amazon_reviews where product_title SIMILAR TO ‘L%’ order by customer_id  LIMIT 20;
+select customer_id , product_title , review_id  from amazon_reviews where product_title similar to 'L%' order by customer_id  limit 20;
 
-select customer_id , product_title  from amazon_reviews where review_body SIMILAR TO ‘%(good|bad)%’;
+select customer_id , product_title  from amazon_reviews where review_body similar to '%(good|bad)%';
 
 select count(*) from amazon_reviews
- where product_title SIMILAR TO  '%lap%' or
-         product_title SIMILAR TO  '%iph%'  or review_body SIMILAR TO '%not%'; 
+ where product_title similar to  '%lap%' or
+       product_title similar to  '%iph%'  or review_body similar to '%not%'; 
 
-select  count(*) from amazon_reviews where product_title SIMILAR TO '%comp%' or product_title SIMILAR TO  '%iph%';
+select  count(*) from amazon_reviews where product_title similar to '%comp%' or product_title similar to  '%iph%';
 
 select  count(*) from amazon_reviews 
-where product_title like  '%lap%' or
-      product_title like  '%iph%' or 
-      product_title like  '%soa%' or 
-      product_title like  '%nice%' or 
+where product_title like '%lap%' or
+      product_title like '%iph%' or 
+      product_title like '%soa%' or 
+      product_title like '%nice%' or 
       product_title like '%hope%';
 
  select  product_title || 'Test ' from amazon_reviews where product_title ilike '%lap%' or product_title ilike '%e%|%E%' or customer_ID like '3%__%45__3';
